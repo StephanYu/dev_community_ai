d23c0a807c4ea5df2989304f3b65d80a4d77c825
StephanYu <stephan.yu@gmail.com>
2023-10-05 10:03:45 +0200

    Add themeswitcher, shadcn ui components, navbar

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -6,24 +6,31 @@ interface ThemeContextType {
   theme: string;
   setTheme: (theme: string) => void;
 }
+
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
-  const [theme, setTheme] = useState("dark");
+  const [theme, setTheme] = useState("");
 
-  const toggleTheme = () => {
-    if (theme === "dark") {
-      setTheme("light");
-      document.documentElement.classList.add("light");
-    } else {
+  const handleThemeChange = () => {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
       setTheme("dark");
       document.documentElement.classList.add("dark");
+    } else {
+      setTheme("light");
+      document.documentElement.classList.remove("dark");
     }
   };
 
   useEffect(() => {
-    toggleTheme();
+    handleThemeChange();
   }, [theme]);
+
+  // console.log("theme, ", theme);
 
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
@@ -34,8 +41,10 @@ export function ThemeProvider({ children }: { children: React.ReactNode }) {
 
 export function useTheme() {
   const context = useContext(ThemeContext);
+
   if (context === undefined) {
     throw new Error("useTheme must be used within a ThemeProvider");
   }
+
   return context;
 }
