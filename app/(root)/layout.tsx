17898985e741ea4c15b9b5dafe753ce59896f5f8
StephanYu <stephan.yu@gmail.com>
2023-10-06 09:44:54 +0200

    Add RightSideBar, shadcn badge and render tag

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,5 @@
+import LeftSideBar from "@/components/shared/LeftSideBar";
+import RightSideBar from "@/components/shared/RightSideBar";
 import Navbar from "@/components/shared/navbar/Navbar";
 import React from "react";
 
@@ -7,14 +9,14 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <Navbar />
 
       <div className="flex">
-        {/* Left Sidebar */}
+        <LeftSideBar />
 
         {/* Main */}
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
 
-        {/* Right Sidebar */}
+        <RightSideBar />
       </div>
 
       {/* Toaster */}
