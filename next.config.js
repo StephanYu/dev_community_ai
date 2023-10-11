3ec4a2e15c0730a9b3a186dc870a88fa74bbb10d
StephanYu <stephan.yu@gmail.com>
2023-10-11 15:26:17 +0200

    Add author picture to QuestionCard

--- next.config.js
+++ next.config.js
@@ -5,5 +5,17 @@ const nextConfig = {
     mdxRs: true,
     serverComponentsExternalPackages: ["mongoose"],
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+      },
+      {
+        protocol: "http",
+        hostname: "*",
+      },
+    ],
+  },
 };
 module.exports = nextConfig;
